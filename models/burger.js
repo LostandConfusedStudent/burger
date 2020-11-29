c93a943a8e19063f7bc2c6d4693676854712c3e9
pausun <pfsun9999@msn.com>
2020-11-28 23:12:54 -0800

    Worked on burger.js in new models directory

---
+++ models/burger.js
@@ -0,0 +1,29 @@
+var orm = require("../config/orm");
+
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+
+    insertOne: function(column, burgerInput, cb) {
+        orm.selectAll("burgers", column, burgerInput, function(res) {
+            cb(res);
+        });
+    },
+    
+    updateOne: function(objColVals, condition, cb) {
+        orm.selectAll("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    },
+    
+    deleteOne: function(condition, cb) {
+        orm.selectAll("burgers", condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
