233610a3dd13cafd2d054fdbe43081c72e6f9039
pausun <pfsun9999@msn.com>
2020-12-02 20:45:20 -0800

    Working on deploying to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 const mysql = require("mysql");
 
 // Connection properties
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
-    database: "burgers_db"
-});
 
-connection.connect(function(err) {
-    if (err) {
-        console.error("Error connecting: " + err.stack);
-        return;
-    }
-    console.log("Connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
+}
+
+connection.connect();
 
 module.exports = connection;
